225f028f24ae32aac85420f98892e8edbb3de395
Ayisha Alli <alli.ayisha1@gmail.com>
2022-03-31 14:26:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const squares = document.querySelectorAll('.box')
-    const playerDisplay = document.querySelector('#playerDisplay')
+document.addEventListener('DOMContentLoaded', () => { // waits for all HTML content to load
+    const squares = document.querySelectorAll('.box') // selects the game squares 
+    const playerDisplay = document.querySelector('#playerDisplay') // HTML tag 
 
-    let currentPlayer = 'PlayerX'
+    let currentPlayer = 'PlayerX' // current player is assinged PlayerX
 
-    squares.forEach(square =>{
-        square.addEventListener('click', clickOutcome)
+    squares.forEach(square =>{ // loops over each square 
+        square.addEventListener('click', clickOutcome) // adds an action to each square. so everytime its clicked, it will perform the clickOutcome() function
     })
-    function clickOutcome(e){
-        const squareArray = Array.from(squares)
-        const index = squareArray.indexOf(e.target)
-        playerDisplay.innerHTML = currentPlayer;
+    
+    function clickOutcome(e){ // this function takes one parameter
+        const squareArray = Array.from(squares) // creates a new array of the squares and assigns it to squareArray
+        const index = squareArray.indexOf(e.target) // wherever is clicked, will select the index of the squareArray and this has been assigned to index
+        playerDisplay.innerHTML = currentPlayer; // whoever currentPlayer is will be displayed in the HTML 
         
-        if (currentPlayer === 'PlayerX'){
-            currentPlayer = 'Player0'
-            squareArray[index].innerHTML ='<i class="far fa-9x fa-circle"></i>'
+        if (currentPlayer === 'PlayerX'){ // if current player is equal to playerX then 
+            currentPlayer = 'Player0' // the player will switch turns as it means playerX has played alreay
+            squareArray[index].innerHTML ='<i class="far fa-9x fa-circle"></i>' // the square will be filled with a circle to represent Player0 playing 
             
         } else {
             currentPlayer = 'PlayerX'
-            squareArray[index].innerHTML ='<i class="fas fa-10x fa-times"></i>'
+            squareArray[index].innerHTML ='<i class="fas fa-10x fa-times"></i>' // the square will be filled with a circle to represent PlayerX playing 
         }
     }
 })
